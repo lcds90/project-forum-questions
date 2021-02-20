516bbc838d436da2e0897e4380c48c3f7f93e4d9
Leonardo Santos <lcds90@gmail.com>
2021-02-20 16:23:41 -0300

    config json

--- database/database.js
+++ database/database.js
@@ -1,8 +1,17 @@
 // import de lib e parametros para realizar conexão
 const SEQUELIZE = require("sequelize");
-const CONN = new SEQUELIZE(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: 'postgres',
-});
+/* const CONN = new SEQUELIZE("node_projeto_perguntas", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+}); */
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  const CONN = new SEQUELIZE(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
+}
+
 
 module.exports = CONN;
