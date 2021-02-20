ead219ec33d4a7fe8315db7a3a74f4763b15d0bc
Leonardo Santos <lcds90@gmail.com>
2021-02-20 16:01:12 -0300

    database config

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 // import de lib e parametros para realizar conexão
 const SEQUELIZE = require("sequelize");
 const CONN = new SEQUELIZE("ddptofaomo3fdm", "blotcomdrxibro", "7d574d49dc5d703ee4cd31bddcd559a0dfcc6f29ee66ab585a546d70a007970c", {
-  host: DATABASE_URL,
+  host: process.env.DATABASE_URL,
   dialect: "postgres",
 });
 
